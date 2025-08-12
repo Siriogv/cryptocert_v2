0caf6669e3fcc7f8ebdfd331b3229bb4626dd328
Giovanni <82497796+Siriogv@users.noreply.github.com>
2025-08-12 18:27:06 +0200

    Handle empty file search results

--- application/views/admin/filesearch.php
+++ application/views/admin/filesearch.php
@@ -14,7 +14,7 @@ $this->load->model('model_object');
             </nav>
         </div>
 
-<?php if (!empty($files)) { ?>
+<?php if (is_array($certificat) && count($certificat) > 0) { ?>
 <div class="row table-responsive">
             <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name" class="form-control mb-3">
             <table id="myTable" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
