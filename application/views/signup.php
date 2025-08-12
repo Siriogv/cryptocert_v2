6a6edfc1276707419a76d4925a70290d6f682741
Giovanni <82497796+Siriogv@users.noreply.github.com>
2025-08-12 15:04:43 +0200

    feat: use bootstrap via CDN

--- application/views/signup.php
+++ application/views/signup.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="<?=base_url();?>assets/css/style.css">
 </head>
 <body>
     <h1>Sign Up</h1>
